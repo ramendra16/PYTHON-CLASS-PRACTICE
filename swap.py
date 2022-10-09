2e80330b807fc41f24cb2b071057345ce83aaf70
Ramendra Srivastava <aadarsh16041995@gmail.com>
2022-10-09 13:54:44 +0530

    swap function program

---
+++ swap.py
@@ -0,0 +1,8 @@
+#take inputs from user for two numbers(x and y)
+x=input("please enter value for x: ")
+y=input("please enter value for y: ")
+
+x,y=y,x
+
+print ("the value of x after swapping: ",x)
+print ("the value of y after swapping: ",y)
