68558007ff8c9bfa05c2a49b16ab21b6db63a015
Ramendra Srivastava <aadarsh16041995@gmail.com>
2022-10-16 12:17:50 +0530

    adding ifElsequestion file

---
+++ ifElseQuestion.py
@@ -0,0 +1,11 @@
+# USER INPUT \ DIVISIBLE BY 2 AND 3 / TRUE/ IF NOT THEN FALSE
+a= int(input("ENTER YOUR NUMBER"))
+if a%2==0:
+    print("YES IT IS DIVISIBLE BY 2")
+
+elif a%3==0:
+    print("YES IT IS DIVIBLE BY 3")
+
+
+else:
+      print("INVALID INPUT")
