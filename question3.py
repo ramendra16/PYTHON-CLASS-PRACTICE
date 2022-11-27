6500bec8ac55afef7e6030eb22980aad044b183f
Ramendra Srivastava <aadarsh16041995@gmail.com>
2022-11-27 09:25:42 +0530

    question

---
+++ question3.py
@@ -0,0 +1,14 @@
+num1=int(input("enter first number"))
+num2=int(input("enter second number"))
+
+operator=("+,-,*,%")
+ 
+operator1="+"
+operator2="-"
+operator3="*"
+operator4="%"
+
+if num1>num2:
+    print(num1+num2)
+    
+elif
