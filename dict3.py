e9349117eb0df5cdc262e6b237dfeee88119e92c
Ramendra Srivastava <aadarsh16041995@gmail.com>
2022-12-04 13:27:29 +0530

    dict ques

---
+++ dict3.py
@@ -0,0 +1,38 @@
+dict={
+    "class":{
+            "student":{
+                "name":"abc",
+                "marks":{
+                    "python":90,
+                    "web":95
+             }
+        }
+    }
+}
+print(dict["class"]["student"]["marks"]["web"])
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
