11903fa6252b079ed006db70af9cc6d3755623cf
Ramendra Srivastava <aadarsh16041995@gmail.com>
2022-12-04 12:36:58 +0530

    set concept

---
+++ set2.py
@@ -0,0 +1,12 @@
+set1={"car", "boat", "jeep", "car"}
+#duplicate not allowed
+set2={1,2,3,4}
+set3={4,5,6,7}
+
+
+#output= set2.intersection(set3)
+output2= set2.symmetric_difference(set3)
+#print(output2)
+print(output2)
+#set1.discard(set2)
+#print(set1)
