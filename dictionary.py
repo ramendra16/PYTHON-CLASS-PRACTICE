106753c1cecb75c22295a89ade55ff681a3453fe
Ramendra Srivastava <aadarsh16041995@gmail.com>
2022-12-04 12:46:28 +0530

    dictionary

---
+++ dictionary.py
@@ -0,0 +1,12 @@
+#dictionary are use to store data values in key value pair
+
+myDictionary={
+"name":"ramendra",
+"age":19,
+"age":21,
+"class": 13,
+"percentage":100,
+"roll_num":50
+
+}
+print(myDictionary)
