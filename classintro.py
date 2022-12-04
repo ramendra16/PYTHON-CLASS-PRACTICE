c933da2d19969d7c8d6765dc5bbdf15a4324576a
Ramendra Srivastava <aadarsh16041995@gmail.com>
2022-12-04 16:39:16 +0530

    class intro

---
+++ classintro.py
@@ -0,0 +1,15 @@
+# x=5
+# y="6"
+# print(type(y))
+
+
+    #attributes and behaviour
+class laptop:
+    def config(self):
+        print("ryzen5", "1tb", "16gb")
+
+laptop1=laptop()
+laptop2=laptop()
+
+laptop1.config() 
+laptop2.config()
